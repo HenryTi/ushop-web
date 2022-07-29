e9a2feedf7e1ec178702ef8b1b2a79ebfe11bb81
HenryTi <liaohengyi@gmail.com>
2022-07-29 11:37:55 -0400

    ok

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,60 +16,14 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
 
         </Head>
         <header>
-            <nav className="navbar static-top navbar-light bg-light container">
-                <div className="container">
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="/about">
-                        <a>About</a>
-                    </Link>{' '}
-                    |{' '}
-                    <Link href="/users">
-                        <a>Users List</a>
-                    </Link>{' '}
-                    | <a href="/api/users">Users API</a>
-                </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
+            header
         </header>
         <div className="container">
             {children}
         </div>
         <footer>
             <hr />
-            <span>I'm here to stay (Footer)</span>
+            <span>I am here to stay (Footer)</span>
         </footer>
     </div>
 )
